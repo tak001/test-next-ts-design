3b10f81ce96e8a7ac4afbf985e8d3f0dd508b2c1
taikou-kobayashi-mg <taiko.kobayashi@m-gild.com>
2022-05-29 10:14:47 +0900

    feat:recoil-rendering-test

--- infrastructure/provider/axiosBase.ts
+++ infrastructure/provider/axiosBase.ts
@@ -1,18 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { objectKeysToCamel, objectKeysToKebab } from '@/utils/changeCase';
 
-const existsWindow = typeof window !== 'undefined';
-const headers = existsWindow
-  ? {
-      'X-Origin': location.hostname,
-      'X-Protocol': location.protocol,
-    }
-  : undefined;
-
 const client = axios.create({
-  // ブラウザのSessionCookieなどをaxiosでそのままサーバーに送信する
   withCredentials: true,
-  headers,
 });
 
 client.interceptors.request.use(
