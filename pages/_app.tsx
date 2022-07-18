a1ae527e0e9f58ad34ba674824c0e98b2698c378
taikou-kobayashi-mg <taiko.kobayashi@m-gild.com>
2022-07-18 14:24:14 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-import DefaultLayout from '@/layouts/Default';
+import DefaultLayout from '@/layouts/default';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
